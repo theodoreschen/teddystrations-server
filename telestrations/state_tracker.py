d58f9b33881515e6dd41ae2843a2be069b59650f
theodoreschen <theodoreschen@gmail.com>
2020-04-23 01:02:48 -0400

    [skip ci] AbstractStateTracker supports timers

--- telestrations/state_tracker.py
+++ telestrations/state_tracker.py
@@ -2,17 +2,37 @@ from abc import (
     ABC,
     abstractmethod
 )
+from .utils import GameState
+from uuid import UUID
 
 
 class AbstractStateTracker(ABC):
+    _game_rounds: int
+    _current_round: int
+    _viewing_uuid: UUID
+    _admin_uuid: UUID
+
     @abstractmethod
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def setState(self, state: str) -> bool:
+    def setState(self, state: GameState) -> bool:
         return True
 
     @abstractmethod
-    def getState(self) -> str:
-        return ''
+    def getState(self) -> GameState:
+        return GameState.UNKNOWN
+
+    @abstractmethod
+    def timerStart(self, duration: int=60):
+        return
+
+    @abstractmethod
+    def timerStop(self):
+        return
+
+    @abstractmethod
+    def timerTimeRemaining(self) -> int:
+        return -1
+
