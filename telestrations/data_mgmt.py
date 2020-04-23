56493f05a8901d7bd3dd624905a458a8cd385594
theodoreschen <theodoreschen@gmail.com>
2020-04-23 11:54:52 -0400

    [skip ci] more methods for data_mgmt

--- telestrations/data_mgmt.py
+++ telestrations/data_mgmt.py
@@ -2,9 +2,31 @@ from abc import (
     ABC,
     abstractmethod
 )
+from uuid import UUID
+from .data_types import Player
 
 
 class AbstractDataMgmt(ABC):
     @abstractmethod
     def __init__(self, *args, **kwargs):
         pass
+
+    @abstractmethod
+    def add_player(self, name) -> Player:
+        return None
+
+    @abstractmethod
+    def get_player(self, uid: str) -> Player:
+        return None
+
+    @abstractmethod
+    def delete_player(self, uid: str) -> Player:
+        return None
+
+    @abstractmethod
+    def add_content(self, uid: str, content: str):
+        return
+
+    @abstractmethod
+    def retrieve_content(self, uid: str):
+        return
