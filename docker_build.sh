0121e63e4c93c12b9cf141dd40a2ef04b3697224
tsc <theairportexplorer@gmail.com>
2020-06-05 18:49:04 -0400

    using v1.1 of ng-teddystrations-game and player

--- docker_build.sh
+++ docker_build.sh
@@ -3,13 +3,13 @@
 rm -rf static/
 mkdir static
 
-curl -sL https://github.com/theodoreschen/ng-teddystrations-game/releases/download/1.0/ng-teddystrations-game.tgz --output ng-teddystrations-game.tgz
+curl -sL https://github.com/theodoreschen/ng-teddystrations-game/releases/download/v1.1/ng-teddystrations-game.tgz --output ng-teddystrations-game.tgz
 tar xzvf ng-teddystrations-game.tgz
 mv ng-teddystrations-game/* static/.
 mv static/index.html static/game.html
 rmdir ng-teddystrations-game
 
-curl -sL https://github.com/theodoreschen/ng-teddystrations-players/releases/download/1.0/ng-teddystrations-players.tgz --output ng-teddystrations-players.tgz
+curl -sL https://github.com/theodoreschen/ng-teddystrations-players/releases/download/v1.1/ng-teddystrations-players.tgz --output ng-teddystrations-players.tgz
 tar xzvf ng-teddystrations-players.tgz
 mv ng-teddystrations-players/* static/.
 mv static/index.html static/player.html
